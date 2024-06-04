7befc7e1a549f79a88a5617b507f10b254e1d62d
Mibo <meebsawosika@gmail.com>
2024-06-03 23:55:48 -0600

    Created operate function that does math on string

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const add = function(a,b) {
-	return a + b;
+	return +a + +b;
 };
 
 const subtract = function(a,b) {
@@ -13,3 +13,34 @@ const divide = function(a,b) {
 const multiply = function(a,b) {
   return a*b;
 };
+
+let firstCalcValue, secondCalcValue, operator;
+
+firstCalcValue = prompt();
+operator = prompt();
+secondCalcValue = prompt();
+
+
+const operate = function(firstNum,secondNum,operate) {
+    let operation;
+	switch (operate) {
+		case "+":
+			operation = add(firstNum,secondNum);
+		break;
+
+		case "-":
+			operation = subtract(firstNum,secondNum);
+		break;
+
+		case "/":
+			operation = divide(firstNum,secondNum);
+		break;
+
+		case "*":
+			operation = multiply(firstNum,secondNum);
+		break;
+	}
+	return operation;
+}
+
+console.log(operate(firstCalcValue,secondCalcValue,operator));
